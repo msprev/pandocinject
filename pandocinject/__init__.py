42acb1f86bbb1ed1f5db6eb6954bad12ef4b837b
Mark Sprevak <sprevak.home@gmail.com>
2015-11-24 21:04:00 +0000

    export classes at top level

---
+++ pandocinject/__init__.py
@@ -0,0 +1,4 @@
+# make these classes available as top level imports from package
+from pandocinject.pandocinject import Injector
+from pandocinject.formatter import Formatter
+from pandocinject.selector import Selector
